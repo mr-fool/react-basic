832b794de5937d77fbd68e9a6c7269b5a3902b0b
mr-fool <mr-fool@users.noreply.github.com>
2019-06-21 21:15:05 -0600

    new object

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+const user = {
+    name: "cyka",
+    lastname: "blyat",
+    age: 100
+}
+
 const Header = () => {
-    return <div>This is my new Header</div>
+    return (
+        <div>
+            <div>{user.name}</div>
+            <div>{user.lastname}</div>
+        </div>
+
+    ) 
 }
 
 export default Header;
