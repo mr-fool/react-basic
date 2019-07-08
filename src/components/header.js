630d5f0b0f683f9f8be0c98f0021bd28d83433f2
mr-fool <mr-fool@users.noreply.github.com>
2019-07-08 14:58:03 -0600

    back to react

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,10 @@
 import React from "react";
 
-const user = {
-    name: "cyka",
-    lastname: "blyat",
-    age: 100
-}
 
 const Header = () => {
     return (
         <div>
-            <div>{user.name}</div>
-            <div>{user.lastname}</div>
-            <div>{user.age}</div>
+            Header
         </div>
 
     ) 
