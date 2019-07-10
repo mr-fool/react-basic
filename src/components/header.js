5ecd2c4ca0ae579e681381f31f8b6f05c269173c
mr-fool <mr-fool@users.noreply.github.com>
2019-07-09 19:35:55 -0600

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,17 @@
 import React, {Component} from "react";
 import "../css/styles.css"
 
+
+
 class Header extends Component {
     render() {
 
         return (
             <header>
-                <div className="logo">Logo</div>
+                <div 
+                    className="logo"
+                    onClick = {()=> {console.log("I was clicked")}}
+                    >Logo</div>
                 <input type="text"/>
             </header>
 
